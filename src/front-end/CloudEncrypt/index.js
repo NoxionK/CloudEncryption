76efd05a0ccf532b26416ddcdd54fbbff6010d07
HaiTuyen <nguyenhaituyen1804@gmail.com>
2024-03-10 17:19:35 +0700

    Delete CloudEncrypt folder

--- src/front-end/CloudEncrypt/index.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelector('.my-button');
-button.addEventListener('click', () => {
-    console.log('Button was clicked!');
-    window.sendAPI.send('open-error-dialog');
-});
-
-window.listenAPI.receive('opened-error-dialog', (data) => {
-    console.log(data);
-});
